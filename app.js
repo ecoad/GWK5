6186d6065d4a499146291eca5a285b777f8c6269
ecoad <elliotcoad@gmail.com>
2012-06-26 21:03:45 +0100

    Demo for sending snapshot to server

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express = require('express')
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', { layout: true });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
--- bundles/sandbox/controller.js
+++ bundles/sandbox/controller.js
@@ -1,13 +1,25 @@
-var urlPrefix = '/sandbox/';
+var urlPrefix = '/sandbox/'
+  , snapshots = [];
 
 module.exports.createRoutes = function(serviceLocator, bundleViewPath) {
   [
-    { path: urlPrefix + 'webcam-capture', action: getHomeController, method: 'get' }
+    { path: urlPrefix + 'webcam-capture', action: getWebcamCaptureController, method: 'get' }
+  , { path: urlPrefix + 'post-to-server', action: getPostToServer, method: 'get' }
+  , { path: urlPrefix + 'post-to-server', action: postPostToServer, method: 'post' }
   ].forEach(function(route) {
     serviceLocator.app[route.method](route.path, route.action);
   });
 
-  function getHomeController(req, res) {
+  function getWebcamCaptureController(req, res) {
     res.render(bundleViewPath + '/webcam-capture');
+  }
+
+  function getPostToServer(req, res) {
+    res.render(bundleViewPath + '/post-to-server', {snapshots: snapshots.slice(0, 10).reverse()});
+  }
+
+  function postPostToServer(req, res) {
+    snapshots.push(req.body.data);
+    res.send('ok');
   }
 };
